bd9f80cead0dd7b3b1649c2073e28b31c11abe83
Sanjarbek <154433378+Sanjarbek-007@users.noreply.github.com>
2024-05-24 17:01:54 +0500

    Update reference.sql

--- Lesson24/reference.sql
+++ Lesson24/reference.sql
@@ -12,3 +12,30 @@ CREATE TABLE books (
     FOREIGN KEY (author_id) REFERENCES authors(author_id) ON DELETE SET NULL
 );
 
+INSERT INTO authors (name) VALUES 
+('Sanjarbek Jorayev'), 
+('Fahriddin Karimov'), 
+('Begzod Islomov'), 
+('Diyorbek Abdurahmonov'), 
+('Hamidjon Usmonov'), 
+('Nurmuhammad Ergashev'), 
+('Ali Rahmonov'), 
+('Vali Toshmatov');
+
+-- Books jadvaliga ma'lumotlar kiritish
+INSERT INTO books (name, pages, author_id) VALUES 
+('Qahraton', 250, 1), 
+('Jazirama', 300, 2), 
+('Yolgizlik', 150, 3), 
+('Iztirob', 200, 4), 
+('Yoshlar Uchun', 220, 5), 
+('Daryo Sadosi', 180, 6), 
+('Quvonch', 260, 7), 
+('Xotiralar', 190, 8), 
+('Asrlar Sadosi', 300, 1), 
+('Jaholatga Qarshi', 310, 2), 
+('Ozodlik', 280, 3), 
+('Insoniyat', 270, 4), 
+('Kelajak', 240, 5), 
+('Tarix', 330, 6), 
+('Sarguzasht', 290, 7);
