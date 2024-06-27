0198c94c851cc91e3320082ad8b75f6da3cb4e75
Sanjarbek <sanjarbekabdurahmonov123@gmail.com>
2024-06-27 12:16:59 +0500

    homework46

---
+++ lesson46/migrations/000001_create_tables2.up.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS weather (
+    name VARCHAR NOT NULL,
+    temperature INTEGER,
+    humidity REAL,
+    windSpeed INTEGER,
+    condition VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS transport (
+    name VARCHAR NOT NULL,
+    number INTEGER NOT NULL ,
+    
+)
